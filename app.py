7ffacaadc4a7b9181e94947c93a440dd6d0cf6fc
Trng191 <138800454+Trng191@users.noreply.github.com>
2024-08-13 21:23:00 +0700

    app.py

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app)  # Thêm dòng này để bật CORS
+CORS(app)  # Bật CORS
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024 * 10  # 100 GB
 
 # Đảm bảo thư mục uploads tồn tại
 if not os.path.exists(UPLOAD_FOLDER):
@@ -13,15 +14,26 @@ if not os.path.exists(UPLOAD_FOLDER):
 
 @app.route('/')
 def home():
-    return render_template('test.html')
+    return render_template('app.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    file = request.files['file']
-    if file:
-        filename = file.filename
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return 'File uploaded successfully'
+    file = request.files.get('file')
+    filename = request.form.get('filename')
+    start = int(request.form.get('start'))
+    end = int(request.form.get('end'))
+
+    if file and filename:
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        if not os.path.exists(file_path):
+            with open(file_path, 'wb') as f:
+                f.write(b'\0' * end)  # Tạo file trống với kích thước end
+
+        with open(file_path, 'r+b') as f:
+            f.seek(start)
+            f.write(file.read())
+
+        return 'File chunk uploaded successfully'
     return 'No file uploaded', 400
 
 @app.route('/download/<filename>', methods=['GET'])
